71d7530e06be5f0cbe33e788abc5124eb62009ba
philg <philgreening@hotmail.com>
2022-04-13 23:17:10 +0100

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,67 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import * as ScreenOrientation from 'expo-screen-orientation';
 
 export default function App() {
+  ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.LANDSCAPE_LEFT);
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+
+    <SafeAreaView style={styles.safeArea}>
+
+        <Text style={styles.welcomeText}>Hello</Text>
+        <Text style={styles.subtitleText}>my name is</Text>
+        <View style={styles.box}>
+          <Text style={styles.nameText}>Phil (he/him) </Text>
+        </View>
+
+      </SafeAreaView >
       <StatusBar style="auto" />
+
     </View>
+
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'red',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  welcomeText: {
+    fontSize: 90,
+    textTransform: 'uppercase',
+    fontWeight: 'bold',
+    color: 'white',
+    textAlign: 'center'
+},
+subtitleText: {
+    fontSize: 30,
+    textTransform: 'uppercase',
+    fontWeight: 'bold',
+    color: 'white',
+    marginBottom: 20,
+    textAlign: 'center'
+},
+box: {
+  width: '90%',
+  height: '55%',
+  backgroundColor: 'white',
+  borderRadius: 5,
+  justifyContent: 'center',
+  margin: 'auto'
+},
+nameText: {
+  fontSize: 60,
+  textAlign: 'center',
+  fontWeight: 'bold'
+},
+safeArea: {
+  flex: 1,
+  width: '100%',
+  height: '100%',
+
+}
 });
